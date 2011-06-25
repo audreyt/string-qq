a0d643cdec52e1ca827935639f7785fa8ed2381c
Audrey Tang <audrey.tang@socialtext.com>
2011-06-26 03:42:41 +0800

    * Rename string-quote to string-qq.

--- src/Data/String/QQ.hs
+++ src/Data/String/QQ.hs
@@ -2,32 +2,35 @@
 
 -- | QuasiQuoter for non-interpolated strings, texts and bytestrings.
 --
--- The "s" quoter contains a multi-line string with no interpolation at all:
+-- The "s" quoter contains a multi-line string with no interpolation at all,
+-- except that the leading newline is trimmed and carriage returns stripped.
 --
 -- @
 -- {-\# LANGUAGE QuasiQuotes #-}
 -- import Data.Text (Text)
--- import Data.String.Quote
+-- import Data.String.QQ
 -- foo :: Text -- "String", "ByteString" etc also works
 -- foo = [s|
--- 
 -- Well here is a
 --     multi-line string!
--- 
 -- |]
 -- @
 --
 -- Any instance of the IsString type is permitted.
---
-module Data.String.Quote (s) where
+-- 
+module Data.String.QQ (s) where
 import GHC.Exts (IsString(..))
 import qualified Language.Haskell.TH as TH
 import Language.Haskell.TH.Quote
 
 -- | QuasiQuoter for a non-interpolating IsString literal. The pattern portion is undefined.
 s :: QuasiQuoter
-s = QuasiQuoter ((\a -> [|fromString a|]) . filter (/= '\r'))
+s = QuasiQuoter ((\a -> [|fromString a|]) . trimLeadingNewline . removeCRs)
                  (error "Cannot use q as a pattern")
                  (error "Cannot use q as a type")
                  (error "Cannot use q as a dec")
+    where
+    removeCRs = filter (/= '\r')
+    trimLeadingNewline ('\n':xs) = xs
+    trimLeadingNewline xs = xs
 
--- tests/Test.hs
+++ tests/Test.hs
@@ -3,21 +3,26 @@
 module Main where
 
 import Data.Text
-import Data.String.Quote
+import Data.String.QQ
 import Test.HUnit
-
-data Foo = Foo Int String deriving Show
-
-t1 = "字元"
 
 test0 = assertBool "" ([s||] == "")
 test1 = assertBool "" ([s|1|] == "1")
-test2 = assertBool "" ([s|2|] == pack "1")
+test2 = assertBool "" ([s|2|] == pack "2")
+test0' = assertBool "" ([s|
+|] == "")
+test1' = assertBool "" ([s|
+1|] == "1")
+test2' = assertBool "" ([s|
+2|] == pack "2")
 
 tests = TestList
     [ TestLabel "Empty String" $ TestCase test0
     , TestLabel "String instance" $ TestCase test1
     , TestLabel "Text instance" $ TestCase test2
+    , TestLabel "Empty String with trimmed leading newline" $ TestCase test0'
+    , TestLabel "String instance with trimmed leading newline" $ TestCase test1'
+    , TestLabel "Text instance with trimmed leading newline" $ TestCase test2'
     ]
 
 main = runTestTT tests
